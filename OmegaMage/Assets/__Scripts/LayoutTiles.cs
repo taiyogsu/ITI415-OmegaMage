092ff859b97a9f5b9498122b820bb84517c2a098
Evan Law <evanlaw.el@gmail.com>
2016-11-19 23:02:57 -0500

    First Update
    Finished up to "Selecting Elements"

---
+++ OmegaMage/Assets/__Scripts/LayoutTiles.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class TileTex
+{
+    // This class enables us to define various textures for tiles
+    public string str;
+    public Texture2D tex;
+}
+
+public class LayoutTiles : MonoBehaviour
+{
+    static public LayoutTiles S;
+
+    public TextAsset roomsText; // The Rooms.xml file
+    public string roomNumber = "0"; // Current room # as a string
+    // ^ roomNumber as string allows encoding in the XML & rooms 0-F
+    public GameObject tilePrefab; // Prefab for all Tiles
+    public TileTex[] tileTextures; // A list of named textures for Tiles
+
+    public bool ________________;
+
+    public PT_XMLReader roomsXMLR;
+    public PT_XMLHashList roomsXML;
+    public Tile[,] tiles;
+    public Transform tileAnchor;
+
+    void Awake()
+    {
+        S = this; // Set the Singleton for LayoutTiles
+
+        // Make a new GameObject to be the TileAnchor (the parent transform of
+        //  all Tiles). This keeps Tiles tidy in the Hierarchy pane.
+        GameObject tAnc = new GameObject("TileAnchor");
+        tileAnchor = tAnc.transform;
+
+        // Read the XML
+        roomsXMLR = new PT_XMLReader(); // Create a PT_XMLReader
+        roomsXMLR.Parse(roomsText.text); // Parse the Rooms.xml file
+        roomsXML = roomsXMLR.xml["xml"][0]["room"]; // Pull all the <room>s
+
+        // Build the 0th Room
+        BuildRoom(roomNumber);
+    }
+
+    // This is the GetTileTex() method that Tile uses
+    public Texture2D GetTileTex(string tStr)
+    {
+        // Search through all the tileTextures for the proper string
+        foreach (TileTex tTex in tileTextures)
+        {
+            if (tTex.str == tStr)
+            {
+                return (tTex.tex);
+            }
+        }
+        // Return null if nothing was found
+        return (null);
+    }
+
+
+    // Build a room based on room number. This is an alternative version of
+    // BuildRoom that grabs roomXML based on <room> num.
+    public void BuildRoom(string rNumStr)
+    {
+        PT_XMLHashtable roomHT = null;
+        for (int i = 0; i < roomsXML.Count; i++)
+        {
+            PT_XMLHashtable ht = roomsXML[i];
+            if (ht.att("num") == rNumStr)
+            {
+                roomHT = ht;
+                break;
+            }
+        }
+        if (roomHT == null)
+        {
+            Utils.tr("ERROR", "LayoutTiles.BuildRoom()",
+                     "Room not found: " + rNumStr);
+            return;
+        }
+        BuildRoom(roomHT);
+    }
+
+
+
+    // Build a room from an XML <room> entry
+    public void BuildRoom(PT_XMLHashtable room)
+    {
+        // Get the texture names for the floors and walls from <room> attributes
+        string floorTexStr = room.att("floor");
+        string wallTexStr = room.att("wall");
+        // Split the room into rows of tiles based on carriage returns in the Rooms.xml file
+        string[] roomRows = room.text.Split('\n');
+        // Trim tabs from the beginnings of lines. However, we're leaving spaces
+        //  and underscores to allow for non-rectangular rooms.
+        for (int i = 0; i < roomRows.Length; i++)
+        {
+            roomRows[i] = roomRows[i].Trim('\t');
+        }
+        // Clear the tiles Array
+        tiles = new Tile[100, 100]; // Arbitrary max room size is 100x100
+
+        // Declare a number of local fields that we'll use later
+        Tile ti;
+        string type, rawType, tileTexStr;
+        GameObject go;
+        int height;
+        float maxY = roomRows.Length - 1;
+
+        // These loops scan through each tile of each row of the room
+        for (int y = 0; y < roomRows.Length; y++)
+        {
+            for (int x = 0; x < roomRows[y].Length; x++)
+            {
+                // Set defaults
+                height = 0;
+                tileTexStr = floorTexStr;
+
+                // Get the character representing the tile
+                type = rawType = roomRows[y][x].ToString();
+                switch (rawType)
+                {
+                    case " ": // empty space
+                    case "_": // empty space
+                              // Just skip over empty space
+                        continue;
+                    case ".": // default floor
+                              // Keep type="."
+                        break;
+                    case "|": // default wall
+                        height = 1;
+                        break;
+                    default:
+                        // Anything else will be interpreted as floor
+                        type = ".";
+                        break;
+                }
+
+                // Set the texture for floor or wall based on <room> attributes
+                if (type == ".")
+                {
+                    tileTexStr = floorTexStr;
+                }
+                else if (type == "|")
+                {
+                    tileTexStr = wallTexStr;
+                }
+
+                // Instantiate a new TilePrefab
+                go = Instantiate(tilePrefab) as GameObject;
+                ti = go.GetComponent<Tile>();
+                // Set the parent Transform to tileAnchor
+                ti.transform.parent = tileAnchor;
+                // Set the position of the tile
+                ti.pos = new Vector3(x, maxY - y, 0);
+                tiles[x, y] = ti; // Add ti to the tiles 2D Array
+
+                // Set the type, height, and texture of the Tile
+                ti.type = type;
+                ti.height = height;
+                ti.tex = tileTexStr;
+
+                // If the type is still rawType, continue to the next iteration
+                if (rawType == type) continue;
+
+                // Check for specific entities in the room
+                switch (rawType)
+                {                                          // 1
+                    case "X": // Starting position for the Mage
+                        Mage.S.pos = ti.pos; // Uses the Mage Singleton
+                        break;
+                }
+
+                // More to come here...
+            }
+        }
+    }
+}
---
+++ OmegaMage/Assets/__Scripts/Mage.cs
@@ -0,0 +1,299 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;    // Enables List<>s
+using System.Linq;                   // Enables LINQ queries
+
+// The MPhase enum is used to track the phase of mouse interaction
+public enum MPhase
+{
+    idle,
+    down,
+    drag
+}
+
+// The ElementType enum
+public enum ElementType
+{
+    earth,
+    water,
+    air,
+    fire,
+    aether,
+    none
+}
+
+// MouseInfo stores information about the mouse in each frame of interaction
+[System.Serializable]
+public class MouseInfo
+{
+    public Vector3 loc;         // 3D loc of the mouse near z=0
+    public Vector3 screenLoc;   // Screen position of the mouse
+    public Ray ray;         // Ray from the mouse into 3D space
+    public float time;        // Time this mouseInfo was recorded
+    public RaycastHit hitInfo;     // Info about what was hit by the ray
+    public bool hit;         // Whether the mouse was over any collider
+
+    // These methods see if the mouseRay hits anything
+    public RaycastHit Raycast()
+    {
+        hit = Physics.Raycast(ray, out hitInfo);
+        return (hitInfo);
+    }
+
+    public RaycastHit Raycast(int mask)
+    {
+        hit = Physics.Raycast(ray, out hitInfo, mask);
+        return (hitInfo);
+    }
+}
+
+// Mage is a subclass of PT_MonoBehaviour
+public class Mage : PT_MonoBehaviour
+{
+    static public Mage S;
+    static public bool DEBUG = true;
+
+    public float mTapTime = 0.1f; // How long is considered a tap
+    public GameObject tapIndicatorPrefab; // Prefab of the tap indicator
+
+    public float mDragDist = 5; // Min dist in pixels to be a drag
+
+    public float activeScreenWidth = 1; // % of the screen to use
+
+    public float speed = 2; // The speed at which _Magee walks
+    
+    public bool ________________;
+
+    public MPhase mPhase = MPhase.idle;
+    public List<MouseInfo> mouseInfos = new List<MouseInfo>();
+
+    public bool walking = false;
+    public Vector3 walkTarget;
+    public Transform characterTrans;
+
+    void Awake()
+    {
+        S = this; // Set the Mage Singleton
+        mPhase = MPhase.idle;
+
+        // Find the characterTrans to rotate with Face()
+        characterTrans = transform.Find("CharacterTrans");
+    }
+
+    void Update()
+    {
+        // Find whether the mouse button 0 was pressed or released this frame
+        bool b0Down = Input.GetMouseButtonDown(0);
+        bool b0Up = Input.GetMouseButtonUp(0);
+
+        // Handle all input here (except for Inventory buttons)
+        /*
+        There are only a few possible actions:
+        1. Tap on the ground to move to that point
+        2. Drag on the ground with no spell selected to move to the Mage
+        3. Drag on the ground with spell to cast along the ground
+        4. Tap on an enemy to attack (or force-push away without an element)
+        */
+
+        // An example of using < to return a bool value
+        bool inActiveArea = (float)Input.mousePosition.x / Screen.width < activeScreenWidth;
+
+        // This is handled as an if statement instead of switch because a tap
+        //  can sometimes happen within a single frame
+        if (mPhase == MPhase.idle)
+        { // If the mouse is idle
+            if (b0Down && inActiveArea)
+            {
+                mouseInfos.Clear();  // Clear the mouseInfos
+                AddMouseInfo();      // And add a first MouseInfo
+
+                // If the mouse was clicked on something, it's a valid MouseDown
+                if (mouseInfos[0].hit)
+                {    // Something was hit!
+                    MouseDown();            // Call MouseDown()
+                    mPhase = MPhase.down;   // and set the mPhase
+                }
+            }
+        }
+
+        if (mPhase == MPhase.down)
+        { // if the mouse is down
+            AddMouseInfo(); // Add a MouseInfo for this frame
+            if (b0Up)
+            {     // The mouse button was released
+                MouseTap(); // This was a tap
+                mPhase = MPhase.idle;
+            }
+            else if (Time.time - mouseInfos[0].time > mTapTime)
+            {
+                // If it's been down longer than a tap, this may be a drag, but
+                //  to be a drag, it must also have moved a certain number of
+                //  pixels on screen.
+                float dragDist = (lastMouseInfo.screenLoc -
+                                  mouseInfos[0].screenLoc).magnitude;
+                if (dragDist >= mDragDist)
+                {
+                    mPhase = MPhase.drag;
+                }
+            }
+        }
+
+        if (mPhase == MPhase.drag)
+        { // if the mouse is being drug
+            AddMouseInfo();
+            if (b0Up)
+            {
+                // The mouse button was released
+                MouseDragUp();
+                mPhase = MPhase.idle;
+            }
+            else
+            {
+                MouseDrag(); // Still dragging
+            }
+        }
+    }
+
+    // Pulls info about the Mouse, adds it to mouseInfos, and returns it
+    MouseInfo AddMouseInfo()
+    {
+        MouseInfo mInfo = new MouseInfo();
+        mInfo.screenLoc = Input.mousePosition;
+        mInfo.loc = Utils.mouseLoc; // Gets the position of the mouse at z=0
+        mInfo.ray = Utils.mouseRay; // Gets the ray from the Main Camera through
+                                    //  the mouse pointer
+        mInfo.time = Time.time;
+        mInfo.Raycast(); // Default is to raycast with no mask
+
+        if (mouseInfos.Count == 0)
+        {
+            // If this is the first mouseInfo
+            mouseInfos.Add(mInfo); // Add mInfo to mouseInfos
+        }
+        else
+        {
+            float lastTime = mouseInfos[mouseInfos.Count - 1].time;
+            if (mInfo.time != lastTime)
+            {
+                // if time has passed since the last mouseInfo
+                mouseInfos.Add(mInfo); // Add mInfo to mouseInfos
+            }
+            // This time test is necessary because AddMouseInfo() could be called twice in one frame
+        }
+        return (mInfo); // Return mInfo as well
+    }
+
+    public MouseInfo lastMouseInfo
+    {
+        // Access to the latest MouseInfo
+        get
+        {
+            if (mouseInfos.Count == 0) return (null);
+            return (mouseInfos[mouseInfos.Count - 1]);
+        }
+    }
+
+    void MouseDown()
+    {
+        // The mouse was pressed on something (it could be a drag or tap)
+        if (DEBUG) print("Mage.MouseDown()");
+    }
+
+    void MouseTap()
+    {
+        // Something was tapped like a button
+        if (DEBUG) print("Mage.MouseTap()");
+
+        WalkTo(lastMouseInfo.loc); // Walk to the latest mouseInfo pos
+        ShowTap(lastMouseInfo.loc); // Show where the player tapped
+
+    }
+
+    void MouseDrag()
+    {
+        // The mouse is being drug across something
+        if (DEBUG) print("Mage.MouseDrag()");
+
+        // Continuously walk toward the current mouseInfo pos
+        WalkTo(mouseInfos[mouseInfos.Count - 1].loc);
+    }
+
+    void MouseDragUp()
+    {
+        // The mouse is released after being drug
+        if (DEBUG) print("Mage.MouseDragUp()");
+        // Stop walking when the drag is stopped
+        StopWalking();
+    }
+
+    // Walk to a specific position. The position.z is always 0
+    public void WalkTo(Vector3 xTarget)
+    {
+        walkTarget = xTarget;    // Set the point to walk to
+        walkTarget.z = 0;        // Force z=0
+        walking = true;          // Now the Mage is walking
+        Face(walkTarget);        // Look in the direction of the walkTarget
+    }
+
+    public void Face(Vector3 poi)
+    { // Face toward a point of interest
+        Vector3 delta = poi - pos; // Find vector to the point of interest
+        // Use Atan2 to get the rotation around Z that points the X-axis of
+        //  _Mage:CharacterTrans toward poi
+        float rZ = Mathf.Rad2Deg * Mathf.Atan2(delta.y, delta.x);
+        // Set the rotation of characterTrans (doesn't actually rotate _Mage)
+        characterTrans.rotation = Quaternion.Euler(0, 0, rZ);
+    }
+
+    public void StopWalking()
+    { // Stops the _Mage from walking
+        walking = false;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    void FixedUpdate()
+    { // Happens every physics step (i.e., 50 times/second)
+        if (walking)
+        { // If Mage is walking
+            if ((walkTarget - pos).magnitude < speed * Time.fixedDeltaTime)
+            {
+                // If Mage is very close to walkTarget, just stop there
+                pos = walkTarget;
+                StopWalking();
+            }
+            else
+            {
+                // Otherwise, move toward walkTarget
+                GetComponent<Rigidbody>().velocity = (walkTarget - pos).normalized * speed;
+            }
+        }
+        else
+        {
+            // If not walking, velocity should be zero
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+
+        // Colliding with a wall can also stop walking
+        Tile ti = otherGO.GetComponent<Tile>();
+        if (ti != null)
+        {
+            if (ti.height > 0)
+            { // If ti.height is > 0
+                // Then this ti is a wall, and Mage should stop
+                StopWalking();
+            }
+        }
+    }
+
+    // Show where the player tapped
+    public void ShowTap(Vector3 loc)
+    {
+        GameObject go = Instantiate(tapIndicatorPrefab) as GameObject;
+        go.transform.position = loc;
+    }
+}
